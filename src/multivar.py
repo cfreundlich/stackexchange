0b6886d7cdb701bc1fdba68e3a3bcd1bfe96693c
Charles Freundlich <cfreundlich@tesla.com>
2022-04-24 22:32:32 -0700

    added scatter plot of solutions

--- src/multivar.py
+++ src/multivar.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import math
 import itertools
 import numpy as np
@@ -66,33 +66,68 @@ def dfz(x, y, z):
     return A * B
 
 
+def gradient(vars):
+    return [dfx(*vars), dfy(*vars), dfz(*vars)]
+
+
 eps = 1e-6
+decimals = 3
 table = [['x', 'y', 'z']]
 
 p = math.pi
 interesting_points = {'0': 0, 'pi/2': p/2, 'pi': p, '3pi/2': 3*p/2}
 permutations = itertools.permutations(interesting_points.items(), 3)
 for (kx, x), (ky, y), (kz, z) in permutations:
-    g = np.array([dfx(x, y ,z), dfy(x, y ,z), dfz(x, y ,z)])
-    if np.linalg.norm(g) < eps:
+    if np.linalg.norm(gradient([x, y ,z])) < eps:
         table.append([kx, ky, kz])
         continue
     
+solutions = set(
+    tuple(round(interesting_points[k], decimals) for k in row) 
+    for row in table[1:])
 
-def gradient(vars):
-    return [dfx(*vars), dfy(*vars), dfz(*vars)]
-
-more_interesting_points = np.arange(0, 2 * math.pi, 0.1)
+more_interesting_points = np.arange(0, 2 * math.pi, 0.25)
 more_permutations = itertools.permutations(more_interesting_points, 3)
 for x, y, z in more_permutations:
 
     solution = fsolve(gradient, (x, y, z))
-    if np.linalg.norm(gradient(solution)) < eps:
-        table.append(solution)
+    if not all(
+        0 < solution[i] < 2 * math.pi
+        for i in [0, 1, 2]
+    ):
+        continue
+    solution = tuple(round(v, decimals) for v in solution)
+    if solution in solutions:
+        continue
+    solutions.add(solution)
+    # table.append(solution)
 
-print(tabulate.tabulate(table[1:], headers=table[0]))
+# print(tabulate.tabulate(table[1:], headers=table[0]))
+
+
+class Points:
+    def __init__(self, solutions_) -> None:
+        self._solutions_ = solutions_
+    def x(self):
+        return [r[0] for r in self._solutions_]
+    def y(self):
+        return [r[1] for r in self._solutions_]
+    def z(self):
+        return [r[2] for r in self._solutions_]
 
 
 
+points = Points(solutions)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(points.x(), points.y(), points.z())
 
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
+ax.set_xlim([0, 2*math.pi])
+ax.set_ylim([0, 2*math.pi])
+ax.set_zlim([0, 2*math.pi])
+
+plt.show()
